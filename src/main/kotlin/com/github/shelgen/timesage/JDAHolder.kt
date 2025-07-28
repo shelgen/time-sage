08a66a189bf6c1a4ecad5b9c3f3ae226119911ca
shelgen <shelgen@users.noreply.github.com>
2025-07-28 18:21:51 +0200

    Removed unnecessary LoggerFactory call

--- src/main/kotlin/com/github/shelgen/timesage/JDAHolder.kt
+++ src/main/kotlin/com/github/shelgen/timesage/JDAHolder.kt
@@ -3,13 +3,11 @@ package com.github.shelgen.timesage
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
-import org.slf4j.LoggerFactory
 import java.io.File
 
 object JDAHolder {
     val jda: JDA = run {
         val botToken = File("time-sage-bot.token").readText()
-        val logger = LoggerFactory.getLogger(javaClass)
         logger.info("Logging on with token")
         val build = JDABuilder.createDefault(botToken)
             .setActivity(Activity.playing(" at being developed"))
