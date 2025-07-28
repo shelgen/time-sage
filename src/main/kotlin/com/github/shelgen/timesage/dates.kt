4a49021308ec2aaf37414afb4a2dd3327324a560
shelgen <shelgen@users.noreply.github.com>
2025-07-28 18:21:12 +0200

    Always consider the "next week" the actual next week

--- src/main/kotlin/com/github/shelgen/timesage/dates.kt
+++ src/main/kotlin/com/github/shelgen/timesage/dates.kt
@@ -2,7 +2,7 @@ package com.github.shelgen.timesage
 
 import java.time.*
 
-fun nextMonday() = ZonedDateTime.now(ZoneOffset.UTC).plusDays(5).toLocalDate().mondayOfSameWeek()
+fun nextMonday() = ZonedDateTime.now(ZoneOffset.UTC).plusDays(7).toLocalDate().mondayOfSameWeek()
 fun weekDatesForMonday(weekMondayDate: LocalDate) = (0L..6L).map(weekMondayDate::plusDays)
 fun LocalDate.mondayOfSameWeek(): LocalDate = minusDays(dayOfWeek.value - 1L)
 fun LocalDate.atNormalStartTime(): OffsetDateTime = atTime(LocalTime.of(18, 30)).atOffset(ZoneOffset.UTC)
