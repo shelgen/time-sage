9b13c18209ffcd73fe062b2abb3bb527a8fb61c7
shelgen <shelgen@users.noreply.github.com>
2025-12-07 16:05:40 +0100

    Remove debug lines for time zone selection

--- src/main/kotlin/com/github/shelgen/timesage/JDAHolder.kt
+++ src/main/kotlin/com/github/shelgen/timesage/JDAHolder.kt
@@ -4,7 +4,6 @@ import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import java.io.File
-import java.util.TimeZone
 
 object JDAHolder {
     val jda: JDA = run {
@@ -14,10 +13,6 @@ object JDAHolder {
             .setActivity(Activity.customStatus("Being developed"))
             .build()
         logger.info("Waiting for JDA to be ready")
-        logger.info(
-            TimeZone.getAvailableIDs()
-                .groupBy { it.substringBefore('/') }.entries.joinToString("\n") { it.key + ": " + it.value.size })
-        logger.info("${TimeZone.getAvailableIDs().size} total zones")
         build.awaitReady()
     }
 }
